1e49d8aed5d704711f8a01150c88c7cf45d6c2c1
behnoudng <behnoudng@gmail.com>
2025-11-10 20:18:11 +0000

    add a script for processing raw text

---
+++ src/text_preprocessing.py
@@ -0,0 +1,8 @@
+#2
+import re
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from pathlib import Path
