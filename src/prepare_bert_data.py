999b023bc51b50a9bf0eba58e72e19b62407b614
behnoudng <behnoudng@gmail.com>
2025-12-07 13:29:23 +0330

    prepare bert data

---
+++ src/prepare_bert_data.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+train_o = pd.read_csv("data/processed/train.csv")
+test_o = pd.read_csv("data/processed/test.csv")
+
+train_texts, val_texts, train_labels, val_labels = train_test_split(
+    train_o["text"].values,
+    train_o['class'].values,
+    test_size = 0.1,
+    stratify=train_o['class'],
+    random_state=42
+)
+
+bert_train = pd.DataFrame({
+    'text': train_texts,
+    'label': [1 if l == 'suicide' else 0 for l in train_labels]
+})
+
+bert_val = pd.DataFrame({
+    'text': val_texts,
+    'label': [1 if l == 'suicide' else 0 for l in val_labels]
+})
+
+bert_test = pd.DataFrame({
+    'text': test_o['text'].values,
+    'label': [1 if l == 'suicide' else 0 for l in test_o['class'].values]
+})
+
+bert_train.to_csv("data/processed/bert_train.csv", index=False)
+bert_val.to_csv("data/processed/bert_val.csv", index=False)
+bert_test.to_csv("data/processed/bert_test.csv", index=False)
